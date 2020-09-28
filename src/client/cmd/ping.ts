89728d16fb1ccd7d70306e3a8aeb5db4e91ca909
Arun Sivasankaran <arunparthiban@gmail.com>
2020-09-27 23:53:18 -0700

    feat: setup nice command execution architecture as recommended by discord

---
+++ src/client/cmd/ping.ts
@@ -0,0 +1,9 @@
+import Discord from "discord.js";
+
+export const Ping = {
+    name: "ping",
+    description: "ping pong",
+    execute: async (message: Discord.Message, _args: string[]): Promise<void> => {
+        await message.channel.send("pong");
+    },
+};
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,20 +1,38 @@
 import * as Discord from "discord.js";
+
 import conf from "../config";
+import { Commands } from "./cmd";
 
-const client = new Discord.Client();
+class ClientWithCommands extends Discord.Client {
+    public commands: Discord.Collection<string, any>;
 
-client.on("ready", async () => {
-    console.log("client is ready");
+    constructor() {
+        super();
+        this.commands = new Discord.Collection();
+    }
+}
 
-    const guild = await client.guilds.fetch(conf.discord.guild.aruins.id);
-    console.log(`number of members: ${guild.members.cache.array().length}`);
+const client = new ClientWithCommands();
+
+const cmdPrefix = "/";
+
+Commands.forEach((cmd) => {
+    client.commands.set(cmd.name, cmd);
 });
 
-client.on("message", (msg) => {
-    console.log(msg);
-    if (msg.content === "ping") {
-        msg.reply("pong");
+client.on("message", (message) => {
+    if (!message.content.startsWith(cmdPrefix) || message.author.bot) {
+        return;
     }
+
+    const args = message.content.slice(cmdPrefix.length).trim().split(/ +/);
+    const cmd = (args || []).shift()?.toLowerCase() || "";
+
+    if (!client.commands.has(cmd)) {
+        return;
+    }
+
+    client.commands.get(cmd).execute(message, args);
 });
 
 client.on("error", (err) => {
@@ -27,6 +45,13 @@ client.on("shardError", (err) => {
 
 client.on("rateLimit", (rateLimitInfo: Discord.RateLimitData) => {
     console.error("rate limit error", rateLimitInfo);
+});
+
+client.on("ready", async () => {
+    console.log("client is ready");
+
+    const guild = await client.guilds.fetch(conf.discord.guild.aruins.id);
+    console.log(`number of members: ${guild.members.cache.array().length}`);
 });
 
 export default client;
